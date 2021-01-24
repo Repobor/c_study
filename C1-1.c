363ae6451d8bd973a1bc9589dd5712cc9d7ecc5b
Repobor <repobor@qq.com>
2021-01-24 21:19:18 +0800

    C1-2

--- C1-1.c
+++ C1-1.c
@@ -10,6 +10,9 @@
 int main(){
     int i; //声明计次变量
     int j;
+    int k;
+    k = getchar();
+    printf("%d",k);
     char tmp[] = "\n\tVery Good!\n";  //声明字符串变量
     char *ch = NULL; //声明字符串型指针变量 同时置空
     ch = &tmp; //指针变量指向tmp变量
@@ -17,7 +20,7 @@ int main(){
         if(i==31){
             if(ch){
                 for (j=0;j<sizeof(tmp)-1;j++){
-                    printf("%c",*(ch+j));//循环输出 通过偏移
+                    printf("%c",*(ch+j));//循环输出 通过偏移 
                 }
             }
             //以上代码等效 printf("\n\tVery Good!\n");
---
+++ C1-2.c
@@ -0,0 +1,18 @@
+/*
+输入abc三个值，输出其中最大者
+*/
+
+#include <stdio.h>
+
+int main(){
+    int a,b,c,max;
+    printf("please input a,b,c:\n");
+    scanf("%d,%d,%d",&a,&b,&c);
+    //max=a;
+    //三元运算符 (关系表达式) ? 表达式1 : 表达式2;
+    max=(a<b)?b:a;
+    max=(max<c)?c:max;
+    printf("The largest number is %d\n",max);
+    system("pause");
+    return 0;
+}
