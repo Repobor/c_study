205fa2b1e7f7d3a8e4ced3421b53963056fa3261
repobor <repobor@qq.com>
2021-01-27 23:34:37 +0800

    main

--- C1-1.c
+++ C1-1.c
@@ -10,22 +10,21 @@
 int main(){
     int i; //声明计次变量
     int j;
-    char tmp[] = "\n\tVery Good!\n";  //声明字符串变量
-    char *ch = NULL; //声明字符串型指针变量 同时置空
-    ch = &tmp; //指针变量指向tmp变量
-    for(i=0;i<63;i++){
-        if(i==31){
-            if(ch){
-                for (j=0;j<sizeof(tmp)-1;j++){
-                    printf("%c",*(ch+j));//循环输出 通过偏移 
+    char tmp[] = "\n\tVery Good!\n";  // 声明字符串变量
+    char *ch   = NULL; // 声明字符串型指针变量 同时置空
+    ch = &tmp; // 指针变量指向tmp变量
+    for (i = 0; i < 63; i++){
+        if (i == 31){
+            if (ch){
+                for (j = 0; j < sizeof(tmp)-1; j++){
+                    printf ("%c",*(ch+j));// 循环输出 通过偏移 
                 }
             }
-            //以上代码等效 printf("\n\tVery Good!\n");
-        }else{
-            printf("*");
+            // 以上代码等效 printf("\n\tVery Good!\n");
+        }else {
+            printf ("*");
         }
     }
-    printf("\n");
-    system("pause");
+    printf ("\n");
     return 0;
 }
--- C1-2.c
+++ C1-2.c
@@ -6,13 +6,12 @@
 
 int main(){
     int a,b,c,max;
-    printf("please input a,b,c:\n");
-    scanf("%d,%d,%d",&a,&b,&c);
+    printf ("please input a,b,c:\n");
+    scanf ("%d,%d,%d", &a,&b,&c);
     //max=a;
     //三元运算符 (关系表达式) ? 表达式1 : 表达式2;
-    max=(a<b)?b:a;
-    max=(max<c)?c:max;
-    printf("The largest number is %d\n",max);
-    system("pause");
+    max = (a<b) ? b:a;
+    max = (max<c) ? c:max;
+    printf ("The largest number is %d\n", max);
     return 0;
 }
